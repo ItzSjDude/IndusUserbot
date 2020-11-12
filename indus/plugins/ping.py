4ee087a333be38023e98e0d675fb3145f526a266
ѕρι∂у <68327188+sppidy@users.noreply.github.com>
2020-11-12 22:07:30 +0530

    I Dont Know What to do

--- indus/plugins/ping.py
+++ indus/plugins/ping.py
@@ -2,34 +2,6 @@ from telethon import events
 from datetime import datetime
 from indus.utils import admin_cmd, sudo_cmd, eor
 
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
-
-
 @indus.on(admin_cmd(pattern="ping", outgoing=True))
 @indus.on(sudo_cmd(pattern="ping", allow_sudo=True))
 async def _(event):
